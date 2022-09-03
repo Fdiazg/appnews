0a37c4bc1f5c919b6c95c9c70776c07d51af9337
Felipe Diaz <f.diazgaray@gmail.com>
2022-09-03 10:39:20 -0400

    share03092022

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Share } from '@capacitor/share';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-article',
@@ -8,11 +10,13 @@ import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
   styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
-
   @Input() article: Article;
   @Input() index: number;
 
-  constructor(private iab: InAppBrowser) { }
+  constructor(
+    private iab: InAppBrowser,
+    private actionSheetCtrl: ActionSheetController
+  ) {}
 
   ngOnInit() {}
 
@@ -22,4 +26,40 @@ export class ArticleComponent implements OnInit {
     browser.show();
   }
 
+  async openMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'options',
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          handler: () => this.shareArticle(),
+        },
+        {
+          text: 'Favorites',
+          icon: 'heart-outline',
+          handler: () => this.onToggleFavorite(),
+        },
+        {
+          text: 'Cancel',
+          icon: 'close-outline',
+          role: 'cancel',
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
+  async shareArticle() {
+    // console.log('share article');
+    await Share.share({
+      title: this.article.title,
+      text: this.article.source.name,
+      url: this.article.url
+    });
+  }
+
+  onToggleFavorite() {
+    console.log('share article');
+  }
 }
